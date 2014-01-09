c39fba6d8ef665910cf38b199ceb6dba45be8836
oboroc <aoboroc@gmail.com>
2014-01-08 20:20:51 -0500

    Add Open Watcom C32 1.9 build script

---
+++ src/build-wc19.cmd
@@ -0,0 +1,12 @@
+@echo off
+echo Compile asmsx with Open Watcom C32 1.9
+
+SET PATH=C:\WATCOM\BINW;C:\WATCOM\BINNT;%PATH%
+SET INCLUDE=C:\WATCOM\H;C:\WATCOM\H\NT
+SET WATCOM=C:\WATCOM
+
+wcc386.exe bdecli.c
+echo.
+link386.exe bdecli.obj
+
+if exist *.obj del *.obj
