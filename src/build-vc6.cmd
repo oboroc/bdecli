2315f72dd70691779604ac700fdc7553adbb6cec
oboroc <aoboroc@gmail.com>
2014-01-11 18:00:24 -0500

    Add /nologo to cl.exe

--- src/build-vc6.cmd
+++ src/build-vc6.cmd
@@ -6,7 +6,7 @@ set INCLUDE=%VCDIR%\VC98\INCLUDE;..\..\uthash\src
 set LIB=%VCDIR%\VC98\LIB
 set PATH=%VCDIR%\Common\msdev98\bin;%VCDIR%\VC98\bin;%PATH%
 
-cl.exe /G6 /W4 bdecli.c > bdecli.vc6.err
+cl.exe /nologo /G6 /W4 bdecli.c > bdecli.vc6.err
 rem /W1 to /W4 for warnings, /WX to treat warning as errors, /O2 for moderate optimization
 
 if exist *.obj del *.obj
