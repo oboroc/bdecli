607ec018e73d2ffaf6c51c1ab1600bae4f353f07
oboroc <aoboroc@gmail.com>
2014-01-12 18:30:54 -0500

    Optimize for size and generate Pentium Pro code
    dmc.exe -o+space -6

--- src/build-dmc.cmd
+++ src/build-dmc.cmd
@@ -5,7 +5,7 @@ SET PATH=C:\dm\bin;%PATH%
 SET INCLUDE=C:\dm\include;..\..\compat_s\src
 set LIB=C:\dm\lib
 
-dmc.exe -A -r -w- bdecli.c
+dmc.exe -A -r -w- -o+space -6 bdecli.c
 
 if exist *.obj del *.obj
 if exist *.map del *.map
