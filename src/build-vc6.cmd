049b9ab7168f507859b21700d7c8d339d1757e6e
oboroc <aoboroc@gmail.com>
2014-01-20 21:59:06 -0500

    Add /TC /O2 to cl.exe
    also don't delete *.i anymore

--- src/build-vc6.cmd
+++ src/build-vc6.cmd
@@ -6,7 +6,7 @@ set INCLUDE=%VCDIR%\VC98\INCLUDE;..\..\compat_s\src
 set LIB=%VCDIR%\VC98\LIB
 set PATH=%VCDIR%\Common\msdev98\bin;%VCDIR%\VC98\bin;%PATH%
 
-cl.exe /nologo /G6 /W4 bdecli.c > bdecli.vc6.err
+cl.exe /nologo /TC /G6 /W4 /O2 bdecli.c > bdecli.vc6.err
 rem /W1 to /W4 for warnings, /WX to treat warning as errors, /O2 for moderate optimization
 
-if exist *.obj del *.obj *.i
+if exist *.obj del *.obj
