22ab2699e65d148868b4c787fe0bb3d770ba63cc
Adrian Oboroc <oboroc@users.noreply.github.com>
2019-04-02 16:02:19 -0400

    Update bdecli.c

--- src/bdecli.c
+++ src/bdecli.c
@@ -777,7 +777,7 @@ int main(int argc, char *argv[]) {
   cfg = NULL;
 
   if (1 == argc) {
-    printf_s("bdecli %s (c) 2010-2018 Adrian Oboroc https://github.com/oboroc/bdecli/ (%s)\n\n", BDECLI_VER, __DATE__);
+    printf_s("bdecli %s (c) 2010-2019 Adrian Oboroc https://github.com/oboroc/bdecli/ (%s)\n\n", BDECLI_VER, __DATE__);
     printf_s(
       "Export settings from idapi32.cfg: bdecli -e idapi32.cfg output.txt\n"
       "Edit the text file, leaving only settings relevant to deployed application.\n"
