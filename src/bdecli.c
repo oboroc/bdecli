808e697d6826563c51bab56e2267a819d003cea3
Adrian Oboroc <oboroc@users.noreply.github.com>
2019-04-02 15:44:21 -0400

    Switch from Apache 2.0 to GPLv3 license

--- src/bdecli.c
+++ src/bdecli.c
@@ -1,25 +1,10 @@
 /* bdecli - Borland Database Engine command-line interface.
  * 
- * Copyright (C) 2010-2018 Adrian Oboroc
+ * Copyright (c) 2010-2019 Adrian Oboroc
  *
- * This file is part of bdecli project
+ * This file is part of bdecli project: https://github.com/oboroc/bdecli/
  *
- *     https://github.com/oboroc/bdecli/
- *
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * See LICENSE file for GPLv3 details
  */
 
 
